4ae29eaa58b01b569a6c04ad7916c25a364eb5c2
Vukas Pajic <vulepajic@gmail.com>
2021-10-13 16:49:40 +0200

    added plugins folder

---
+++ Plugins/IFC_Mapping/__init__.py
@@ -0,0 +1,56 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTIBILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+import bpy
+from bpy.props import StringProperty
+from . import ui, prop, operators
+
+bl_info = {
+    "name" : "IFC Bulk Property Editor",
+    "author" : "Vukas Pajic",
+    "description" : "",
+    "blender" : (2, 80, 0),
+    "version" : (0, 0, 1),
+    "location" : "",
+    "warning" : "",
+    "category" : "Generic"
+}
+
+classes = (
+    prop.PropertiesToBeMapped, 
+    ui.IFCPsetEditor, 
+    ui.IFCParameterMapping, 
+    ui.IFCBulkPropertyAdd, 
+    operators.AddPropertyToBeMapped,
+    operators.RemovePropertyToBeMapped,
+    operators.ClearList,
+    operators.ApplyMapping,
+    operators.BulkAddPsets)
+
+def register():
+    from bpy.utils import register_class
+    for cls in classes:
+        register_class(cls)
+
+    bpy.types.Scene.propertiestobemapped = bpy.props.CollectionProperty(type=prop.PropertiesToBeMapped)
+
+
+def unregister():
+    del bpy.types.Scene.propertiestobemapped
+
+    from bpy.utils import unregister_class
+    for cls in reversed(classes):
+        unregister_class(cls)
+
+
+'''if __name__ == "__main__":
+    register()     '''   
