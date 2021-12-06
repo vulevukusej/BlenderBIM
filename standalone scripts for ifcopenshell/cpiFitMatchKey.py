5b9b7517b4220538d8629836fd4d45738787a6c4
Vukas Pajic <vulepajic@gmail.com>
2021-12-06 15:31:42 +0100

    refactor matchkey generator

--- standalone scripts for ifcopenshell/cpiFitMatchKey.py
+++ standalone scripts for ifcopenshell/cpiFitMatchKey.py
@@ -12,42 +12,46 @@ json_filepath = "C:/Users/vpaji/OneDrive/1. Professional/10. Programming/Python/
 ifc = IfcStore.get_file()
 porr_json = json.load(open(json_filepath, encoding="utf-8"))
 
+allElements = ifc.by_type("IfcElement")
 
-allBuildingElements = ifc.by_type("IfcBuildingElement")
-
-for element in allBuildingElements:
+for element in allElements:
     psets = ifcopenshell.util.element.get_psets(element)
     
-    for pset in psets:
+    for pset in psets:        
         if "porr" not in pset:
             continue
         matchkey_list = {}
         type = pset.split("_")[1]
         bauteil = pset.split("_")[2]
         
-        json_parameters = porr_json[type]["bauteile"][bauteil]["parameters"]
-        for parameter in json_parameters:
-            setkey = json_parameters[parameter]["setkey"]
+        porr_parameters = porr_json[type]["bauteile"][bauteil]["parameters"]
+        autoparameters = porr_json[type]["bauteile"][bauteil]["autoparameters"]
+   
+        for key, val in porr_parameters.items():
+            setkey = val["setkey"]
             if setkey == "False":
                 continue
-            revit_name = json_parameters[parameter]["revit_name"]
-            sort = json_parameters[parameter]["sort"]
-            
+            revit_name = val["revit_name"]
+            prop_value = psets[pset].get(revit_name)
+            sort = val["sort"]
             if revit_name == "ri:Ignore":
-                matchkey = json_parameters[parameter]["values"]["matchkeyonly"]
+                matchkey = list(val["value:matchkey"].values())[0]
             else:
-                obj_value = psets[pset][revit_name]
-                matchkey = json_parameters[parameter]["values"][obj_value]["matchkey"]
-                
+                matchkey = val["value:matchkey"][prop_value]
             matchkey_list[sort] = matchkey
             
-            matchkey_order = sorted(matchkey_list)            
-            cpiFitMatchKey_list = []
+        matchkey_order = sorted(matchkey_list)            
+        cpiFitMatchKey_list = []
 
-            for key in matchkey_order:
-                cpiFitMatchKey_list.append(matchkey_list[key]["matchkey"])
+        for index in matchkey_order:
+            cpiFitMatchKey_list.append(matchkey_list[index])
         
+        properties_to_add = {
+            "cpiFitMatchKey": "_".join(cpiFitMatchKey_list), 
+            **{k:v for k,v in autoparameters.items()}
+            }
         
-        ifcopenshell.api.run("pset.edit_pset", ifc, pset=ifc.by_id(psets[pset]["id"]), properties={"cpiFitMatchKey": "_".join(cpiFitMatchKey_list)})
+        ifcopenshell.api.run("pset.edit_pset", ifc, pset=ifc.by_id(psets[pset]["id"]), properties=properties_to_add)
         Data.load(IfcStore.get_file(), element.id())
 
+print("finished")
