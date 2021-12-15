5ad8cf320ce8e5fb5347778d79ed401e7ddcd2b8
Vukas Pajic <vulepajic@gmail.com>
2021-12-15 15:03:59 +0100

    incremental change

--- standalone scripts for ifcopenshell/name_to_guid.py
+++ standalone scripts for ifcopenshell/name_to_guid.py
@@ -1,12 +1,19 @@
 import ifcopenshell
 
-filepath = "C:/Users/vpaji/OneDrive/Documents/Blender/Test Projects/17-Brita CT1_Taunusstein-V7_Optimized.ifc"
-ifc = ifcopenshell.open(filepath)
+filepath = "C:/Users/vpaji/OneDrive/Documents/Blender/Test Projects/17-Brita CT1_Taunusstein-V7_Optimized"
+ifc = ifcopenshell.open(f"{filepath}.ifc")
 
 elements = ifc.by_type("IfcElement")
 
 for element in elements:
     element.Name = element.GlobalId
     
-gltf = "C:/Users/vpaji/OneDrive/Documents/Blender/Test Projects/17-Brita CT1_Taunusstein-V7_Optimized_gltf.ifc"
-ifc.write(gltf)
+gltf = f"{filepath}_edited_for_gltf"
+ifc.write(f"{gltf}.ifc")
+
+print("finished")
+
+vukas = ifc.create_entity("IFCPROPERTYSETTEMPLATE")
+test = vukas.get_info
+
+
